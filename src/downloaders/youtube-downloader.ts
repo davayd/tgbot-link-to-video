ee16b62c19dc8e90bc54ba205b9d55ad699b9681
Dmitry Dreko <dmitry@acterys.com>
2024-10-18 10:44:47 +0200

    fix: update youtube format

--- src/downloaders/youtube-downloader.ts
+++ src/downloaders/youtube-downloader.ts
@@ -1,10 +1,10 @@
 import ytdlp from "yt-dlp-exec";
 
 export async function ytdlpDownloadVideo(url: string, output: string) {
-  output = output + ".mp4";
   return await ytdlp(url, {
     output: output,
     noPlaylist: true,
     restrictFilenames: true,
+    format: "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
   });
 }
